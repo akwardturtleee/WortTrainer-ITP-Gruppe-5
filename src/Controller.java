112c19fa22eb6ab3e1abe1359b8d939cc086890f
fzan@student.tgm.ac.at <fzan@student.tgm.ac.at>
2025-02-19 22:36:29 +0100

    Hab bisschen was in View gemacht mal so grundstruktur

--- src/Controller.java
+++ src/Controller.java
@@ -3,54 +3,7 @@ import java.io.IOException;
 
 public class Controller {
 
-	private Fragenpool fragenpool;
-
-	private QuizModus quizModus;
-
-	private Nutzer nutzer;
-
-	private View view;
-
-	public Controller {
-		this.fragenpool = new Fragenpool();
-		this.quizModus = new QuizModus(fragenpool);
-		this.view = new View();
-	}
-
-	public void starteQuiz() {
-
-	}
-
-	public void verarbeiteAntwort(String eingabe) {
-
-	}
-
-	public void nutzerRegisterien(String name, String email, String passwort) {
-
-	}
-
-	public void ladeFragenpool(String dateipfad) {
-
-		try {
-			System.out.println("Fragepool erfolgreich geladen.");
-		} catch (IOException){
-			System.err.println("Fehler beim Laden" + e.getMessage());
-		}
-	}
-
-	public void speichereFragenpool(String dateipfad) {
-		try {
-			System.out.println("Fragepool erfolgreich gespeichert.");
-		} catch (IOException e) {
-			System.err.println("Fehler beim Speichern vom Fragepool:" + e.getMessage());
-		}
-	}
-	public void start() {
-		view.startMenü();
-	}
-
-	public static void main (String[]args) {
-		Controller controller = new Controller();
-		controller.start();
+	public static void main(String[] args) {
+		new View();
 	}
 }
--- src/View.java
+++ src/View.java
@@ -1,21 +1,31 @@
 package src;
 
-public class View {
+import javax.swing.*;
+import java.awt.*;
 
-	public void zeigeHauptmenü() {
+public class View extends JFrame {
+	private JFrame frame;
+	public View() {
+		frame = new JFrame("Worttrainer");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800, 600);
+		frame.setLayout(new BorderLayout(10,10));
 
-	}
+		JMenuBar menuBar = new JMenuBar();
 
-	public void zeigeQuizInterface() {
+		JMenu beispiel1 = new JMenu("Beispiel1");
+		JMenu beispiel2 = new JMenu("Beispiel2");
+		JMenu beispiel3 = new JMenu("Beispiel3");
 
-	}
+		JMenuItem beispiel1Item = new JMenuItem("FlorianHATeinen40cmBBC");
 
-	public void zeigeErgebnisse() {
+		beispiel1.add(beispiel1Item);
 
-	}
-
-	public void zeigeBenutzeroptionen() {
-
+		menuBar.add(beispiel1);
+		menuBar.add(beispiel2);
+		menuBar.add(beispiel3);
+		frame.setJMenuBar(menuBar);
+		frame.setVisible(true);
 	}
 
 }
