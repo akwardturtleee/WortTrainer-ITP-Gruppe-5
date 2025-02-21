756c50e23d2e2733f87874e9d7c69b4e26a3f2f5
fzan@student.tgm.ac.at <fzan@student.tgm.ac.at>
2025-02-21 07:58:47 +0100

    View fertig (ohne Logik)

--- src/Nutzer.java
+++ src/Nutzer.java
@@ -4,34 +4,4 @@ import javax.swing.*;
 
 public class Nutzer extends JFrame {
 
-	private String benutzerID;
-
-	private String name;
-
-	private String sprachniveau;
-
-	private String email;
-
-	private String passwort;
-
-	private boolean benachrichtigungen;
-
-	private View view;
-
-	public void kontoErstellen() {
-
-	}
-
-	public void kontoVerwalten() {
-
-	}
-
-	public String fortschrittAnzeigen() {
-		return null;
-	}
-
-	public void quizmodusAktivieren() {
-
-	}
-
 }
